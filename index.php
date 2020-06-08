d21b4df245fad6400d7b380d289b64b599d8433a
freddy7980hal <57619193+freddy7980hal@users.noreply.github.com>
2020-06-08 11:48:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-header("Location: https://www.automaticlaundry.com/superior-service-solutions/");
-die();
+$myfile = fopen("newfile.txt", "r") or die("Unable to open file!");
+$url=fgets($myfile);
+fclose($myfile);
+header("location:".$url);
+exit();
 ?>
