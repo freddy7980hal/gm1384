9f18204b6173f135ab516d2743777af27d6f0080
freddy7980hal <57619193+freddy7980hal@users.noreply.github.com>
2020-10-03 18:37:25 -0500

    Update and rename site-index.php to index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.edenbrothers.com/store/planting-info.html");
+die();
+?>
--- site-index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: https://www.automaticlaundry.com/superior-service-solutions/");
-die();
-?>
